62dba23b61e59abd53233d4703e829efdf80f258
Alina Rodríguez <alinagriselrodriguez@gmail.com>
2018-05-07 11:16:13 -0500

    home

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+    console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$("#cat").mouseover(function() {
+    console.log("SOBRES");
+});
